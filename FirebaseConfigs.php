5c688520f99beae85b1fef162f95a353b3ccfd45
Ali Abu Saleh <44038201+aliabusaleh@users.noreply.github.com>
2019-04-04 19:14:29 +0300

    login page added
    all the files with login page added

---
+++ FirebaseConfigs.php
@@ -0,0 +1,17 @@
+<?php
+echo'
+<script src="https://www.gstatic.com/firebasejs/5.9.1/firebase.js"></script>
+<script>
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyCKE36oTp-9RtoHikpzfDgs4j-5qg7AuH0",
+        authDomain: "mywebproject-nnu.firebaseapp.com",
+        databaseURL: "https://mywebproject-nnu.firebaseio.com",
+        projectId: "mywebproject-nnu",
+        storageBucket: "mywebproject-nnu.appspot.com",
+        messagingSenderId: "590702512366"
+    };
+    firebase.initializeApp(config);
+</script>'
+
+?>
